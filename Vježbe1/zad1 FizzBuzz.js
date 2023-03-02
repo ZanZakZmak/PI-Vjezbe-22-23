5fd1e460a845a9147d53b2a57342fc61350f211e
ZanZakZmak <58043160+ZanZakZmak@users.noreply.github.com>
2023-03-03 00:00:03 +0100

    Create zad1 FizzBuzz.js

---
+++ Vježbe1/zad1 FizzBuzz.js
@@ -0,0 +1,13 @@
+for (let i = 1; i < 101; i++) {
+  let rezultat = "";
+  if (i % 3 === 0) {
+    rezultat += "Fizz";
+  }
+  if (i % 5 === 0) {
+    rezultat += "Buzz";
+  }
+  if (!(rezultat === "")) {
+    console.log(rezultat);
+    //console.log(i);
+  }
+}
