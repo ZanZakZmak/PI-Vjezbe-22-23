0699872178b57a02d88ff6ab90f03389bd9fe518
ZanZakZmak <58043160+ZanZakZmak@users.noreply.github.com>
2023-03-03 00:10:36 +0100

    created sumaDo100

--- Vježbe1/zad1 FizzBuzz.js
+++ Vježbe1/zad1 FizzBuzz.js
@@ -10,4 +10,7 @@ for (let i = 1; i < 101; i++) {
     console.log(rezultat);
     //console.log(i);
   }
+  if (!(i % 3 === 0 || i % 5 === 0)) {
+    console.log(i);
+  }
 }
---
+++ Vježbe1/zad2.js
@@ -0,0 +1,6 @@
+// zbroji sve br od 1 do 100 koristeci jednu varijablu
+let suma = 0;
+for (let i = 1; i < 101; i++) {
+  suma += i;
+}
+console.log(suma);
